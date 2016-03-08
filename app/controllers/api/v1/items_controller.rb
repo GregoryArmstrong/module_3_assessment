f79d5bb60269e3666e2758c487f8c6838f928307
GregoryArmstrong <intergalacticgentleman@gmail.com>
2016-03-08 09:51:28 -0700

    Add Api::V1::Items#show and Api::V1::Items#destroy
    Added tests to access items#show and find correct item entry, as well as to destroy
    a specific entry.

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -5,8 +5,18 @@ class Api::V1::ItemsController < Api::V1::ApiController
   respond_to :json
 
   def index
-    @items = Item.all
-    binding.pry
+    @items = Item.all.select("name, description, image_url")
+
     respond_with :api, :v1, @items
+  end
+
+  def show
+    @item = Item.select("name, description, image_url").find(params[:id])
+
+    respond_with :api, :v1, @item
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
   end
 end
--- spec/controllers/api/v1/items_controller_spec.rb
+++ spec/controllers/api/v1/items_controller_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::ItemsController, type: :controller do
   scenario "GET Api::V1::Items#Index" do
+    Item.create(name: "testname",
+                description: "testdescription",
+                image_url: "test_url.com"
+                )
     get :index, format: :json
 
     expect(response.status).to eq(200)
@@ -12,5 +16,44 @@ RSpec.describe Api::V1::ItemsController, type: :controller do
     expect(items).to have_content("name")
     expect(items).to have_content("description")
     expect(items).to have_content("image_url")
+    expect(items).to_not have_content("updated_at")
+    expect(items).to_not have_content("created_at")
+  end
+
+  scenario "GET Api::V1::Items#show" do
+    item_1 = Item.create(name: "test_name",
+                         description: "test_description",
+                         image_url: "test_url.com"
+                         )
+    item_2 = Item.create(name: "not_name",
+                         description: "not_description",
+                         image_url: "not_url.com"
+                         )
+    get :show, format: :json, id: item_1.id
+
+    expect(response.status).to eq(200)
+    expect(response.content_type).to eq "application/json"
+
+    item = JSON.parse(response.body)
+
+    expect(item).to have_content("test_name")
+    expect(item).to have_content("test_description")
+    expect(item).to have_content("test_url.com")
+    expect(item).to_not have_content("updated_at")
+    expect(item).to_not have_content("created_at")
+    expect(item).to_not have_content("not_name")
+    expect(item).to_not have_content("not_description")
+    expect(item).to_not have_content("not_url.com")
+  end
+
+  scenario "DELETE Api::V1:Items#destroy" do
+    item_1 = Item.create(name: "test_name",
+                         description: "test_description",
+                         image_url: "test_url.com"
+                         )
+
+    delete :destroy, id: item_1.id
+
+    expect(response.status).to eq(204)
   end
 end
